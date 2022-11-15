8a6cf9d99e0905dca12d0242cc9ca86206fd37d7
Nicholas Gigliotti <ndgigliotti@gmail.com>
2022-11-15 15:29:55 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Reinvent the Wheel hackathon project involving forecasting scrap tire volume and optimizing deliveries to reduce carbon emissions.',
+    author='808s',
+    license='MIT',
+)
